f4c61639120d527662d05fca637b256f5ec39d57
tekinnigar1 <148658555+tekinnigar1@users.noreply.github.com>
2024-02-20 13:39:25 +0300

    Update Pickdatebox.jsx

--- src/components/Pickdatebox.jsx
+++ src/components/Pickdatebox.jsx
@@ -3,22 +3,20 @@ import React from "react";
 function Pickdatebox() {
   return (
     <div className="rectangleDateBox">
-      <div className="leftsideofbox">
-        <div className="pickupplacebox">
-          <p id="pickupofficeP">Pick up Office</p>
-          <div>
-            <img id="locationVector" src="Frame.png" alt="location-vector" />
-            <p id="pickuplocation">Istanbul - Vadistanbul</p>
-          </div>
+      <div className="pickupplacebox">
+        <p id="pickupofficeP">Pick up Office</p>
+        <div>
+          <img id="locationVector" src="Frame.png" alt="location-vector" />
+          <p id="pickuplocation">Istanbul - Vadistanbul</p>
         </div>
-        <div className="pickuptimebox">
-          <p>Pick up time</p>
-          <p id="datepickup"> 01/01/2024</p>
-        </div>
-        <div className="dropoffbox">
-          <p>Drop off time</p>
-          <p id="datedropoff"> 03/01/2024</p>
-        </div>
+      </div>
+      <div className="pickuptimebox">
+        <p>Pick up time</p>
+        <p id="datepickup"> 01/01/2024</p>
+      </div>
+      <div className="dropoffbox">
+        <p>Drop off time</p>
+        <p id="datedropoff"> 03/01/2024</p>
       </div>
       <button id="lookvehiclesBT">Look Vehicles</button>
     </div>
